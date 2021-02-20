8214c46b7c4d820293a6ffada5ea1e61e3c8378c
shirokovnv <shirokovnv@gmail.com>
2021-02-20 16:36:38 +0500

    =Added first tests

--- tests/unit/modules/home/views/home.spec.ts
+++ tests/unit/modules/home/views/home.spec.ts
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils';
-import Home from '@/modules/Home/views/Home.vue';
+import Home from '@/modules/home/views/Home.vue';
 
 describe('Home.vue', () => {
-  it('renders home vue', () => {
+  it('renders home view', () => {
     const wrapper = mount(Home);
     expect(wrapper.text()).toMatch('Ready to create an app?');
   });
