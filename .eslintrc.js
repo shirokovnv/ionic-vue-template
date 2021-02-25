04538ca3068de344422354bbc8e256e5ef0167d9
shirokovnv <shirokovnv@gmail.com>
2021-02-25 11:30:46 +0300

    =Commented prettier/recommended rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    'plugin:prettier/recommended',
+    //'plugin:prettier/recommended',
     'prettier',
     'prettier/vue',
   ],
